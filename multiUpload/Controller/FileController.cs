a98f72c593fe7b87b5a48bda6357f472c2a869c2
Zhibek jibek.070292@gmail.com <jibek.070292@gmail.com>
2019-11-23 04:36:29 +0600

    otpravlat zapros s programmy POSTMAN

---
+++ multiUpload/Controller/FileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using multiUpload.Models;
+
+namespace multiUpload.Controller
+{
+    [Route("api/file")]
+   public class FileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Uploads([FromForm] List<IFormFile> files) {
+
+            try {
+                var result = new List<FileUploadResult>();
+                foreach (var file in files) {
+                  var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images",file.FileName);
+                  var stream = new FileStream(path,FileMode.Create);
+                  file.CopyToAsync(stream);
+                    result.Add(new FileUploadResult() {Name=file.FileName, Length=file.Length });
+                }
+                return Ok(result);
+            
+            } 
+            catch {
+                return BadRequest();
+            }
+
+        }
+    }
+}
---
+++ multiUpload/Models/FileUploadResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace multiUpload.Models
+{
+    public class FileUploadResult
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+    }
+}
